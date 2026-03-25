042c939174ab474e7c9051771ac26047b01b38be
putriagis <putribalqisnurhayati@gmail.com>
2026-03-25 07:18:23 +0700

    Second commit

---
+++ Modul4_103022400081/KodePaket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace Modul4_103022400081
+{
+    internal class KodePaket
+    {
+        private Dictionary<string, string> kodePaket = new Dictionary<string, string>()
+        {
+            {"Basic","P201"},
+            {"Standard","P202"},
+            {"Premium","P203"},
+            {"Unlimited","P204"},
+            {"Gaming","P205"},
+            {"Streaming","P206"},
+            {"Family","P207"},
+            {"Business","P208"},
+            {"Student","P209"},
+            {"Traveler","P210"}
+        };
+        public string getKodePaket(string namaPaket)
+        {
+            return kodePaket[namaPaket];
+        }
+    }
+ 
+}
--- Modul4_103022400081/Program.cs
+++ Modul4_103022400081/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Modul 04");
+﻿using System;
+using Modul4_103022400081;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        KodePaket Kode = new KodePaket();
+
+        Console.WriteLine("Basic : " + Kode.getKodePaket("Basic"));
+        Console.WriteLine("Standard : " + Kode.getKodePaket("Standard"));
+        Console.WriteLine("Premium : " + Kode.getKodePaket("Premium"));
+        Console.WriteLine("Unlimited : " + Kode.getKodePaket("Unlimited"));
+        Console.WriteLine("Gaming : " + Kode.getKodePaket("Gaming"));
+        Console.WriteLine("Streaming : " + Kode.getKodePaket("Streaming"));
+        Console.WriteLine("Family : " + Kode.getKodePaket("Family"));
+        Console.WriteLine("Business : " + Kode.getKodePaket("Business"));
+        Console.WriteLine("Student : " + Kode.getKodePaket("Student"));
+        Console.WriteLine("Traveler : " + Kode.getKodePaket("Traveler"));
+    }
+}
